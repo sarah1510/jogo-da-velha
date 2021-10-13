4053baacab669ce51f2c6c4e21103dfdac370e19
BlVck213 <paulocesarrrpontes@gmail.com>
2021-10-13 10:51:50 -0300

    Adicionando imagens

--- game/game.js
+++ game/game.js
@@ -27,33 +27,22 @@ const casa9 = document.getElementById("casa9")
 
 
 function joga(casa){
-    const container = document.querySelector(".casa")
-    const letra = ``
+  
     const casaClicada = document.getElementById(casa)
     
-   if (casaClicada == `<img src="../img/pirata.png" alt="">` || casaClicada == `<img src="../img/marinha.png" alt="">`){
-   }else{
-        casaClicada === letra
-        if (letra.innerHTML == `<img src="../img/pirata.png" alt="">`){
 
-            letra.innerHTML = `<img src="../img/marinha.png" alt="">`
-            container.appendChild(letra);
+        if (casaClicada != ``){
+           
+            casaClicada.innerHTML = `<img src="../img/marinha.png" alt="">`
+       
+          
+        }else{
 
-        }else if(letra.innerHTML == `<img src="../img/marinha.png" alt="">`){
-
-            letra.innerHTML = `<img src="../img/pirata.png" alt="">`
-            container.appendChild(letra)
         }
    }
 
 
-}
 
-function verifica(){
-    if (((casa1 != '') && (casa2 != '') && (casa3 != '') && (casa1 == casa2) && (casa2 == casa3)) || ((casa1 != '') && (casa5 != '') && (casa9 != '') && (casa1 == casa5) && (casa5 == casa9)) || ((casa1 != '') && (casa4 != '') && (casa7 != '') && (casa1 == casa4) && (casa4 == casa7)) || ((casa4 != '') && (casa5 != '') && (casa6 != '') && (casa4 == casa5) && (casa5 == casa6)) || ((casa7 != '') && (casa8 != '') && (casa9 != '') && (casa7 == casa8) && (casa8 == casa9)) || ((casa2 != '') && (casa5 != '') && (casa8 != '') && (casa2 == casa5) && (casa5 == casa8)) || ((casa3 != '') && (casa6 != '') && (casa9 != '') && (casa3 == casa6) && (casa6 == casa9)) || ((casa7 != '') && (casa5 != '') && (casa3 != '') && (casa7 == casa5) && (casa5 == casa3))){
-        alert('Você ganhou! Parabéns campeão!')
-    }
- }
-  
+
  
   
