b9386606ff3dd60602710a2f4e0fd8fd3d177828
BlVck213 <paulocesarrrpontes@gmail.com>
2021-10-25 23:30:31 -0300

    Quem Comeca

--- game/game.js
+++ game/game.js
@@ -10,62 +10,38 @@ const casa6 = document.getElementById("casa6")
 const casa7 = document.getElementById("casa7")
 const casa8 = document.getElementById("casa8")
 const casa9 = document.getElementById("casa9")
+  
+const campoEstaVazio = (casaClicada) => casaClicada.children.length == 0
 
 const winMessage = document.getElementById("winMessage")
 
-
+const tipoDeJogo = document.getElementById("tipoDeJogo")
+const quemComeca = document.getElementById("QuemComeca")
 
 var temVencedor = false
+var ePirata = true
 
+let casaClicada
+  
 
-function casasIguais(casaA, casaB, casaC){
-
-        
-    
-        if((casaA == `<img src="../img/Pirata.png">`) && (casaA == casaB) && (casaB == casaC) ){
-            
-            temVencedor = true
-            document.querySelector(".win").style.display = "flex";
-            winMessage.innerHTML = `<h1>Pirata venceu</h1>
-                                    <button onclick="reiniciar()">Reiniciar Jogo</button>`
-        }else if ((casaA == `<img src="../img/Marinha.png">`) && (casaA == casaB) && (casaB == casaC) ){
-         
-            temVencedor = true
-            document.querySelector(".win").style.display = "flex";
-            winMessage.innerHTML = `<h1>Marinha venceu</h1>
-                                    <button onclick="reiniciar()">Reiniciar Jogo</button>`
-        } else if ((casaA == `<img src="../img/Marinha.png">`) || (casaA == `<img src="../img/Pirata.png">`) && (casaA != casaB) && (casaB != casaC) && (casaA != `` && casaB != `` && casaC != ``)){
-            temVencedor = true
-            document.querySelector(".win").style.display = "flex";
-            winMessage.innerHTML = `<h1>Empate</h1>
-                                    <button onclick="reiniciar()">Reiniciar Jogo</button>`
+    function quemVaiComecar(){
+        if(quemComeca.value == 'Pirata'){
+            ePirata = true
+            console.log(quemComeca.value)
+        } else if(quemComeca.value == 'Marinha') {
+            ePirata = false
+            console.log(quemComeca.value)
         }
- 
-   
-}
+    }
 
-
-
-
-   
-
-    let casaClicada
-    const campoEstaVazio = (casaClicada) => casaClicada.children.length == 0
-
-    const quemComeca = document.getElementById("QuemComeca").value
-    var ePirata = true
 
     function joga(casa){
-  
+
     const casaClicada = document.getElementById(casa)
     const vezDe = document.getElementById("vezDe")
 
-
     let jogador  
-
     let adversario
-
-   
 
     if (campoEstaVazio(casaClicada) && !temVencedor) {
 
@@ -91,17 +67,18 @@ function casasIguais(casaA, casaB, casaC){
         }
 
         vezDe.innerHTML = `Vez de ${adversario}`
+
+     
   
     } 
 
 } 
 
-// const tipoDeJogo = document.getElementById("tipoDeJogo").value
-
-// if (tipoDeJogo = 'playerVSplayer'){
-//     quemComeca.style.disa
-// }
-
+function verificaTipoJogo(){
+    if (tipoDeJogo.value = 'playerVSplayer'){
+        
+    }
+}
 function MenuOpcoes(){
     document.querySelector(".optionsBack").style.display = "flex"
 }
@@ -110,10 +87,45 @@ function FecharOpcoes(){
     document.querySelector(".optionsBack").style.display = "none"
 }
 
-function reiniciar() { 
-    window.location.reload();
+function reiniciarJogo() { 
+    casa1.innerHTML = `` 
+    casa2.innerHTML = ``
+    casa3.innerHTML = ``
+    casa4.innerHTML = ``
+    casa5.innerHTML = ``
+    casa6.innerHTML = ``
+    casa7.innerHTML = ``
+    casa8.innerHTML = ``
+    casa9.innerHTML = ``
+    document.querySelector(".win").style.display = "none"   
+    temVencedor = false
+    quemVaiComecar()
+    vezDe.innerHTML = `Vez de ${quemComeca.value}`
 }
 
-document.getElementById("options").addEventListener("click", MenuOpcoes)
-document.getElementById("close").addEventListener("click", FecharOpcoes)
-document.getElementById('reiniciar').addEventListener("click" , reiniciar)
+function casasIguais(casaA, casaB, casaC){
+
+    if( (casaA == casaB) && (casaB == casaC) ){
+        
+    if(casaA == `<img src="../img/Pirata.png">`){
+        temVencedor = true
+        document.querySelector(".win").style.display = "flex";
+        winMessage.innerHTML = `<h1>Pirata venceu</h1>
+                                <button onclick="reiniciarJogo()">Reiniciar Jogo</button>`
+    }else if (casaA == `<img src="../img/Marinha.png">`){
+     
+        temVencedor = true
+        document.querySelector(".win").style.display = "flex";
+        winMessage.innerHTML = `<h1>Marinha venceu</h1>
+                                <button onclick="reiniciarJogo()">Reiniciar Jogo</button>`
+    } 
+
+    }else if((casa1.innerHTML != ``)  && (casa2.innerHTML != ``) && (casa3.innerHTML != ``) && (casa4.innerHTML != ``) && (casa5.innerHTML != ``) && (casa6.innerHTML != ``) && (casa7.innerHTML != ``) && (casa8.innerHTML != ``) && (casa9.innerHTML != ``)    ){
+        temVencedor = true    
+        document.querySelector(".win").style.display = "flex";
+        winMessage.innerHTML = `<h1>Empate</h1>
+                                <button onclick="reiniciarJogo()">Reiniciar Jogo</button>`
+    }   
+}
+
+
