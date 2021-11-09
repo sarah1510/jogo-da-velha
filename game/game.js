d114d90b419cf5d262e454336005f1c461449ba8
ブラック <79909779+BlVck213@users.noreply.github.com>
2021-11-09 10:26:08 -0300

    Gambitos

--- game/game.js
+++ game/game.js
@@ -252,22 +252,47 @@ function verificaTipoeJoga(){
     
         }  
     
-       
-        if (casa1.innerHTML == `<img src="../img/${jogador}.png">` && casa9.innerHTML == `<img src="../img/${jogador}.png">`) {
-            casa8.innerHTML = `<img src="../img/${adversario}.png">`;
-            botVez = false;
-        }  
-
-        if (casa1.innerHTML == `<img src="../img/${jogador}.png">` && casa6.innerHTML == `<img src="../img/${jogador}.png">`) {
-            casa2.innerHTML = `<img src="../img/${adversario}.png">`;
-            botVez = false;
-        } 
-
-        if (casa3.innerHTML == `<img src="../img/${jogador}.png">` && casa7.innerHTML == `<img src="../img/${jogador}.png">`) {
-            casa8.innerHTML = `<img src="../img/${adversario}.png">`;
-            botVez = false;
+        if(botVez == true){
+            if (casa1.innerHTML == `<img src="../img/${jogador}.png">` && casa9.innerHTML == `<img src="../img/${jogador}.png">`) {
+                casa8.innerHTML = `<img src="../img/${adversario}.png">`;
+                botVez = false;
+            }  
         }    
 
+        if(botVez == true){
+            if (casa1.innerHTML == `<img src="../img/${jogador}.png">` && casa6.innerHTML == `<img src="../img/${jogador}.png">`) {
+                casa2.innerHTML = `<img src="../img/${adversario}.png">`;
+                botVez = false;
+            } 
+        }    
+
+        if(botVez == true){
+            if (casa3.innerHTML == `<img src="../img/${jogador}.png">` && casa4.innerHTML == `<img src="../img/${jogador}.png">`) {
+                casa2.innerHTML = `<img src="../img/${adversario}.png">`;
+                botVez = false;
+            } 
+        }    
+        
+        if(botVez == true){
+            if (casa4.innerHTML == `<img src="../img/${jogador}.png">` && casa9.innerHTML == `<img src="../img/${jogador}.png">`) {
+                casa8.innerHTML = `<img src="../img/${adversario}.png">`;
+                botVez = false;
+            } 
+        } 
+
+        if(botVez == true){
+            if (casa6.innerHTML == `<img src="../img/${jogador}.png">` && casa7.innerHTML == `<img src="../img/${jogador}.png">`) {
+                casa8.innerHTML = `<img src="../img/${adversario}.png">`;
+                botVez = false;
+            } 
+        } 
+
+        if(botVez == true){
+            if (casa3.innerHTML == `<img src="../img/${jogador}.png">` && casa7.innerHTML == `<img src="../img/${jogador}.png">`) {
+                casa8.innerHTML = `<img src="../img/${adversario}.png">`;
+                botVez = false;
+            }    
+        }        
         if (botVez == true) {
             jogarAleatoriamente(verificarAdversario())
         }
